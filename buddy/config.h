f90a203f843ffe636d115b7cb683298a11144c01
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-11-09 15:34:07 -0500

    Set version to 2.5

--- buddy/config.h
+++ buddy/config.h
@@ -4,17 +4,17 @@
 #define MAJOR_VERSION 2
 
 /* BuDDy's minor version. */
-#define MINOR_VERSION 4
+#define MINOR_VERSION 5
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "buddy"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "buddy 2.4"
+#define PACKAGE_STRING "buddy 2.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "buddy"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.4"
+#define PACKAGE_VERSION "2.5"
 
