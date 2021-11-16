1386eba3ec064e6a1081c873dd6450e43638cf17
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-11-16 07:31:44 -0500

    Created regression suite

---
+++ regression/regress.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+make 01-unsat2.ldata
+make 01-unsat2.bdata
+make 02-xorImpliesOr3.ldata
+make 02-xorImpliesOr3.bdata
+make 03-chew-10.bdata
+make 04-chess-32.sdata
+make 05-pigeon-sinz-32.sdata
+make 06-urquhart-li-09.bdata
