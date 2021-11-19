c99695f962d0476b5454472d5f0377fa7ac05ef9
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-11-19 12:37:38 -0500

    Better reporting

--- regression/regress.sh
+++ regression/regress.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+make superclean
+
 make 01-unsat2.ldata
 make 01-unsat2.bdata
 make 02-xorImpliesOr3.ldata
@@ -9,4 +11,5 @@ make 04-chess-32.sdata
 make 05-pigeon-sinz-32.sdata
 make 06-urquhart-li-09.bdata
 
+echo "\nBelow, should get 8 lines of 'VERIFIED'"
 grep VERIFIED *data
