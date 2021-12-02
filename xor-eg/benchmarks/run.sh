44e7a70fd9a46c40fdb99a8e5b6f2220c6798bd1
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-12-01 22:25:27 -0500

    Run xtree benchmark

---
+++ xor-eg/benchmarks/run.sh
@@ -0,0 +1,46 @@
+
+make N=010
+make tree-010.kdata
+make tree-010.cdata
+make tree-010.fcdata
+
+make N=015
+make tree-015.kdata
+make tree-015.cdata
+make tree-015.fcdata
+
+make N=020
+make tree-020.kdata
+make tree-020.cdata
+make tree-020.fcdata
+
+make N=025
+make tree-025.kdata
+make tree-025.cdata
+make tree-025.fcdata
+
+make N=030
+make tree-030.kdata
+make tree-030.cdata
+make tree-030.fcdata
+
+make N=035
+make tree-035.kdata
+make tree-035.cdata
+make tree-035.fcdata
+
+make N=040
+make tree-040.kdata
+make tree-040.cdata
+make tree-040.fcdata
+
+make N=045
+make tree-045.kdata
+make tree-045.cdata
+make tree-045.fcdata
+
+make N=050
+make tree-050.kdata
+make tree-050.cdata
+make tree-050.fcdata
+
