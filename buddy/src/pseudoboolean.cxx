04a06b5e19096c477ef8be63d13b4f019d232920
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-12-02 12:04:00 -0500

    Fixed buffer overflow bug

--- buddy/src/pseudoboolean.cxx
+++ buddy/src/pseudoboolean.cxx
@@ -2,10 +2,10 @@
 #include "prover.h"
 #include <queue>
 
-#define BUFLEN 256
+#define BUFLEN 2048
 
 // For formatting information
-char buf[BUFLEN];
+char ibuf[BUFLEN];
 
 /*
   Sort integers in ascending order
@@ -122,8 +122,8 @@ xor_constraint::xor_constraint(ilist vars, int p, tbdd &vfun) {
     phase = p;
     bdd xfun = bdd_build_xor(variables, phase);
     if (verbosity_level >= 2) {
-	show_xor_buf(buf, vars, p, BUFLEN);
-	print_proof_comment(2, "Validate BDD node N%d representing Xor constraint %s", bdd_nameid(xfun.get_BDD()), buf);
+	//	show_xor_buf(ibuf, vars, p, BUFLEN);
+	print_proof_comment(2, "Validate BDD node N%d representing Xor constraint", bdd_nameid(xfun.get_BDD()));
     }
     validation = tbdd_validate(xfun, vfun);
     key = priority_key(vars);
@@ -185,29 +185,29 @@ static xor_constraint *xor_sum_list_bf(xor_constraint **xlist, int len) {
     if (len == 0)
 	return new xor_constraint();
     // Use breadth-first addition
-    xor_constraint **buf = new xor_constraint*[len];
+    xor_constraint **xbuf = new xor_constraint*[len];
     // Do first level of additions on xlist
     for (int i = 0; i < len-1; i+=2) {
-	buf[i/2] = xor_plus(xlist[i], xlist[i+1]);
+	xbuf[i/2] = xor_plus(xlist[i], xlist[i+1]);
 	delete xlist[i];
 	delete xlist[i+1];
     }
     if (len %2 == 1)
-	buf[(len-1)/2] = xlist[len-1];
+	xbuf[(len-1)/2] = xlist[len-1];
     // Left and right-most positions that have been used in the buffer 
     int left = 0;
     int right = (len-1)/2;
     while (left < right) {
-	xor_constraint *arg1 = buf[left++];
-	xor_constraint *arg2 = buf[left++];
-	buf[++right] = xor_plus(arg1, arg2);
+	xor_constraint *arg1 = xbuf[left++];
+	xor_constraint *arg2 = xbuf[left++];
+	xbuf[++right] = xor_plus(arg1, arg2);
 	delete arg1;
 	delete arg2;
-	if (!buf[right]->is_feasible())
+	if (!xbuf[right]->is_feasible())
 	    break;
     }
-    xor_constraint *sum = buf[right];
-    delete[] buf;
+    xor_constraint *sum = xbuf[right];
+    delete[] xbuf;
     return sum;
 }
 
