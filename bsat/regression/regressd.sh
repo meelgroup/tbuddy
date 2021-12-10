2584a5aebba30648722264e333aa85912b2c6ed4
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-12-09 22:57:58 -0500

    Remove hardest test

--- bsat/regression/regressd.sh
+++ bsat/regression/regressd.sh
@@ -8,9 +8,9 @@ make 01-unsat2.bddata
 make 02-xorImpliesOr3.lddata
 make 02-xorImpliesOr3.bddata
 make 03-chew-10.bddata
-make 04-chess-32.sddata
+#make 04-chess-32.sddata
 #make 05-pigeon-sinz-32.sddata
 #make 06-urquhart-li-09.bddata
 
-echo "\nBelow, should get 6 lines of 'VERIFIED'"
+echo "\nBelow, should get 5 lines of 'VERIFIED'"
 grep VERIFIED *ddata
