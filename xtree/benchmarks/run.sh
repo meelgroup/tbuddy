fb18076aa2e622db8fed41f46d683b94df51a284
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2022-01-02 20:51:29 -0500

    Rename

--- xtree/benchmarks/run.sh
+++ xtree/benchmarks/run.sh
@@ -1,200 +1,44 @@
+#!/bin/sh
+make small N=010
+make small N=020
+make small N=025
+make small N=030
 
-make N=010
-make xtree-010.xdata
-make xtree-010.kdata
-make xtree-010.cdata
-make xtree-010.fcdata
+make medium N=040
+make medium N=050
+make medium N=060
+make medium N=070
+make medium N=080
+make medium N=090
+make medium N=100
+make medium N=200
+make medium N=300
+make medium N=500
 
-make N=015
-make xtree-015.xdata
-make xtree-015.kdata
-make xtree-015.cdata
-make xtree-015.fcdata
+make large N=500
+make large N=600
+make large N=700
+make large N=800
+make large N=900
+make large N=1000
+make large N=2000
+make large N=3000
+make large N=4000
+make large N=5000
+make large N=6000
+make large N=7000
+make large N=8000
+make large N=9000
+make large N=10000
 
-make N=020
-make xtree-020.xdata
-make xtree-020.kdata
-make xtree-020.cdata
-make xtree-020.fcdata
+make huge N=10000
+make huge N=20000
+make huge N=30000
+make huge N=40000
+make huge N=50000
+make huge N=60000
+make huge N=70000
+make huge N=80000
+make huge N=90000
+make huge N=100000
 
-make N=025
-make xtree-025.xdata
-make xtree-025.kdata
-make xtree-025.cdata
-make xtree-025.fcdata
-
-make N=030
-make xtree-030.xdata
-make xtree-030.kdata
-make xtree-030.cdata
-make xtree-030.fcdata
-
-make N=035
-make xtree-035.xdata
-make xtree-035.kdata
-make xtree-035.cdata
-make xtree-035.fcdata
-
-make N=040
-make xtree-040.xdata
-make xtree-040.kdata
-make xtree-040.cdata
-make xtree-040.fcdata
-
-make N=045
-make xtree-040.xdata
-#make xtree-045.kdata
-make xtree-045.cdata
-make xtree-045.fcdata
-
-make N=050
-make xtree-050.xdata
-#make xtree-050.cdata
-make xtree-050.fcdata
-
-make N=055
-make xtree-055.xdata
-make xtree-055.fcdata
-
-
-make N=060
-make xtree-060.xdata
-make xtree-060.fcdata
-
-make N=065
-make xtree-065.xdata
-make xtree-065.fcdata
-
-make N=070
-make xtree-070.xdata
-make xtree-070.fcdata
-
-make N=075
-make xtree-075.xdata
-make xtree-075.fcdata
-
-make N=080
-make xtree-080.xdata
-make xtree-080.fcdata
-
-make N=085
-make xtree-085.xdata
-make xtree-085.fcdata
-
-make N=090
-make xtree-090.xdata
-make xtree-090.fcdata
-
-make N=095
-make xtree-095.xdata
-make xtree-095.fcdata
-
-make N=100
-make xtree-100.xdata
-make xtree-100.fcdata
-
-make N=150
-make xtree-150.xdata
-make xtree-150.fcdata
-
-make N=200
-make xtree-200.xdata
-make xtree-200.fcdata
-
-make N=250
-make xtree-250.xdata
-make xtree-250.fcdata
-
-make N=300
-make xtree-300.xdata
-make xtree-300.fcdata
-
-make N=400
-make xtree-400.xdata
-make xtree-400.fcdata
-
-make N=450
-make xtree-450.xdata
-
-make N=500
-make xtree-500.xdata
-make xtree-500.fcdata
-
-make N=600
-make xtree-600.fcdata
-
-make N=700
-make xtree-700.fcdata
-
-make N=800
-make xtree-800.fcdata
-
-make N=900
-make xtree-900.fcdata
-
-make N=1000
-make xtree-1000.fcdata
-
-make N=2000
-make xtree-2000.fcdata
-
-make N=3000
-make xtree-3000.fcdata
-
-make N=4000
-make xtree-4000.fcdata
-
-make N=5000
-make xtree-5000.fcdata
-
-make N=6000
-make xtree-6000.fcdata
-
-make N=7000
-make xtree-7000.fcdata
-
-make N=8000
-make xtree-8000.fcdata
-
-make N=9000
-make xtree-9000.fcdata
-
-make xtree-010.tdata
-make xtree-015.tdata
-make xtree-020.tdata
-make xtree-025.tdata
-make xtree-030.tdata
-make xtree-035.tdata
-make xtree-040.tdata
-make xtree-045.tdata
-make xtree-050.tdata
-make xtree-055.tdata
-make xtree-060.tdata
-make xtree-065.tdata
-make xtree-070.tdata
-make xtree-075.tdata
-make xtree-080.tdata
-make xtree-085.tdata
-make xtree-090.tdata
-make xtree-095.tdata
-make xtree-100.tdata
-make xtree-150.tdata
-make xtree-200.tdata
-make xtree-250.tdata
-make xtree-300.tdata
-make xtree-400.tdata
-make xtree-500.tdata
-make xtree-600.tdata
-make xtree-700.tdata
-make xtree-800.tdata
-make xtree-900.tdata
-make xtree-1000.tdata
-make xtree-2000.tdata
-make xtree-3000.tdata
-make xtree-4000.tdata
-make xtree-5000.tdata
-make xtree-6000.tdata
-make xtree-7000.tdata
-make xtree-8000.tdata
-make xtree-9000.tdata
-make xtree-10000.tdata
