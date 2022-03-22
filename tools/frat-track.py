d2f9ca9af82b7bec016b191484aa5358d40c60ff
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2022-03-22 11:39:06 -0400

    Print more info

--- tools/frat-track.py
+++ tools/frat-track.py
@@ -9,6 +9,8 @@
 
 import sys
 
+err_max = 50
+err_count = 0
 
 add_commands = ['a', 'o']
 delete_commands = ['d', 'f']
@@ -19,12 +21,19 @@ clause_tracker = {}
 
 
 line_count = 0
+
+cmd_counters = {}
+
 for line in sys.stdin:
     line_count += 1
     fields = line.split()
     if len(fields) < 2 or fields[0] == 'c':
         continue
     cmd = fields[0]
+    if cmd in cmd_counters:
+        cmd_counters[cmd] += 1
+    else:
+        cmd_counters[cmd] = 1
     id = int(fields[1])
     if cmd in add_commands:
         if id in clause_tracker:
@@ -33,16 +42,22 @@ for line in sys.stdin:
             clause_tracker[id] = 1
     elif cmd in delete_commands:
         if id not in clause_tracker:
-            print("Line #%d.  Clause %d not previously defined" % (line_count, id))
+            err_count += 1
+            if err_count <= err_max:
+                print("Line #%d.  Clause %d not previously defined" % (line_count, id))
         elif clause_tracker[id] == 0:
-            print("Line #%d.  Clause %d already accounted for" % (line_count, id))
+            err_count += 1
+            if err_count <= err_max:
+                print("Line #%d.  Clause %d already accounted for" % (line_count, id))
         else:
             clause_tracker[id] -= 1
     elif cmd in reloc_commands:
         id2 = int(fields[2])
         clause_tracker[id] -= 1
         if id2 in clause_tracker and clause_tracker[id] > 0:
-            print("Line #%d. Clause %d is reloced to ID %d but %d is already used" % (line_count, id, id2, id2))
+            err_count += 1
+            if err_count <= err_max:
+                print("Line #%d. Clause %d is reloced to ID %d but %d is already used" % (line_count, id, id2, id2))
         clause_tracker[id2] = 1
     else:
         print("Line #%d.  Unknown command '%s'" % (line_count, cmd))
@@ -53,10 +68,18 @@ left_count = 0
 
 for id in ids:
     if clause_tracker[id] > 0:
+        err_count += 1
         left_count += 1
-        print("Clause %d created but not deleted" % id)
+        if err_count <= err_max:
+            print("Clause %d created but not deleted" % id)
 
-print("%d clauses unaccounted for" % left_count)    
+if left_count > 0:
+    print("%d clauses unaccounted for" % left_count)    
+else:
+    print("All clauses accounted for")    
+if err_count > 0:
+    print("%d errors encountered" % err_count)
 
-        
-    
+print("Command types totals:")
+for cmd in sorted(cmd_counters.keys()):
+    print("   Command:%s %d" % (cmd, cmd_counters[cmd]))
