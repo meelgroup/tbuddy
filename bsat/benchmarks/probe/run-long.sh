2063619e14c7f734658aa39749ae482dfb875319
Randal Bryant <Randy.Bryant@cs.cmu.edu>
2022-04-13 10:54:54 +0200

    Minor tweaks

--- bsat/benchmarks/probe/run-long.sh
+++ bsat/benchmarks/probe/run-long.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-pushd chess-pgbdd; make lprobe ; popd
+#pushd urquhart-bucket; make lprobe ; popd
+#pushd urquhart-gauss; make lprobe ; popd
+#pushd urquhart-pgbdd; make lprobe ; popd
+#pushd urquhart-pgpbs; make lprobe ; popd
 pushd chess-scan; make lprobe ; popd
-pushd chew-bucket; make lprobe ; popd
-pushd chew-gauss; make lprobe ; popd
-pushd chew-pgbdd; make lprobe ; popd
-pushd chew-pgpbs; make lprobe ; popd
+pushd chess-pgbdd; make lprobe ; popd
 pushd pigeon-pgbdd; make lprobe ; popd
 pushd pigeon-scan; make lprobe ; popd
-pushd run-short.sh; make lprobe ; popd
-pushd urquhart-bucket; make lprobe ; popd
-pushd urquhart-gauss; make lprobe ; popd
-pushd urquhart-pgbdd; make lprobe ; popd
-pushd urquhart-pgpbs; make lprobe ; popd
+pushd chew-bucket; make lprobe ; popd
+#pushd chew-gauss; make lprobe ; popd
+pushd chew-pgbdd; make lprobe ; popd
+pushd chew-pgpbs; make lprobe ; popd
--- bsat/benchmarks/probe/run-short.sh
+++ bsat/benchmarks/probe/run-short.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-pushd chess-pgbdd; make sprobe ; popd
-pushd chess-scan; make sprobe ; popd
-pushd chew-bucket; make sprobe ; popd
-pushd chew-gauss; make sprobe ; popd
-pushd chew-pgbdd; make sprobe ; popd
-pushd chew-pgpbs; make sprobe ; popd
+#pushd chess-pgbdd; make sprobe ; popd
+#pushd chess-scan; make sprobe ; popd
+#pushd chew-bucket; make sprobe ; popd
+#pushd chew-gauss; make sprobe ; popd
+#pushd chew-pgbdd; make sprobe ; popd
+#pushd chew-pgpbs; make sprobe ; popd
 pushd pigeon-pgbdd; make sprobe ; popd
 pushd pigeon-scan; make sprobe ; popd
-pushd run-short.sh; make sprobe ; popd
 pushd urquhart-bucket; make sprobe ; popd
 pushd urquhart-gauss; make sprobe ; popd
 pushd urquhart-pgbdd; make sprobe ; popd
