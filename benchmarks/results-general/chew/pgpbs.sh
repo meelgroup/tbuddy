9045c6f401922b856d658da7a62a8bb283fdd212
Randal Bryant <Randy.Bryant@cs.cmu.edu>
2022-07-22 15:51:19 -0400

    More data

--- benchmarks/results-general/chew/pgpbs.sh
+++ benchmarks/results-general/chew/pgpbs.sh
@@ -2,24 +2,24 @@
 
 S=123456
 
-#D=1
-##for DCOUNT in {1..2}
+##D=100
+##for DCOUNT in {3..3}
 ##do
 ##    D=$((D*10)) 
-##    for O in {1..9}
+##    for O in {1..4}
 ##    do
 ##        N=$((D*O))
 ##        make cnf N=$N SEED=$S
-##        make pbdata N=$N SEED=$S
+##        make pgdata N=$N SEED=$S
 ##    done
 ##done
 
 B=1000
 M=100
 
-for O in {0..3}
+for O in {1..9}
 do
     N=$((B+M*O))
     make cnf N=$N SEED=$S
-    make pbdata N=$N SEED=$S
+    make pgdata N=$N SEED=$S
 done
