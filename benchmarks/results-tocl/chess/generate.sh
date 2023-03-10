58100f4a4e98a4c7a1113c9e56cfa61f457cc290
Randal Bryant <Randy.Bryant@cs.cmu.edu>
2023-03-10 18:29:45 -0500

    Refinements

--- benchmarks/results-tocl/chess/generate.sh
+++ benchmarks/results-tocl/chess/generate.sh
@@ -1,38 +1,46 @@
 #!/bin/sh
 
+# scan
+#make schess N=010
+#make schess N=020
+#make schess N=040
+#make schess N=100
+#make schess N=140
+#make schess N=200
+#make schess N=250
+#make schess N=300
+make schess N=340
+#make schess N=350
+
 # bucket
-make bchess N=008
-make bchess N=010
-make bchess N=012
-make bchess N=014
-#make bchess N=016
+#make bchess N=008
+#make bchess N=010
+#make bchess N=012
+#make bchess N=014
+make bchess N=016
+#make bchess N=018
 
 # linear
-make lchess N=008
-make lchess N=010
-make lchess N=012
-make lchess N=014
-#make lchess N=016
+#make lchess N=008
+#make lchess N=010
+#make lchess N=012
+#make lchess N=014
+make lchess N=016
 
 # noquant
-make nqchess N=008
-make nqchess N=010
-make nqchess N=012
-make nqchess N=014
+#make nqchess N=008
+#make nqchess N=010
+ make nqchess N=012
+#make nqchess N=014
 #make nqchess N=016
 
 
 # kissat
-make kchess N=008
-make kchess N=010
-make kchess N=012
-make kchess N=014
-#make kchess N=016
+#make kchess N=008
+#make kchess N=010
+#make kchess N=012
+#make kchess N=014
+make kchess N=016
+#make kchess N=018
 
-# scan
-make schess N=008
-make schess N=016
-make schess N=032
-make schess N=064
-make schess N=128
-make schess N=256
+
