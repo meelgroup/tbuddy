7ab56b42bf7c6ea1e4c7bae4539d1229fe73986c
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2023-03-12 08:18:04 -0400

    Set up experiments

---
+++ benchmarks/results-tocl/urquhart-simon/bucket.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+BS=123456
+
+for M in {3..4}
+do
+    for DS in {0..4}
+    do
+	S=$((BS+DS))
+	make bdata M=$M SEED=$S
+	make rdata M=$M SEED=$S
+    done
+done
+
+
+for M5 in {1..7}
+do
+    M = $((M5*5))
+    for DS in {0..4}
+    do
+	S=$((BS+DS))
+	make bdata M=$M SEED=$S
+	make rdata M=$M SEED=$S
+    done
+done
+
+
+	
